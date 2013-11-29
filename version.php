483189f2505f2fe735caf7bc7ef5487460d9c7c7
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-11-29 16:32:41 +0000

    Version bump for Merthyr changes.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version    = 2013083000;
+$plugin->version    = 2013112900;
 $plugin->requires   = 2010112400;
 $plugin->component  = 'local_leapwebservices';
 $plugin->cron       = 0;
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '0.3.1';
+$plugin->release    = '0.3.2';
